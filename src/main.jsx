1c56e9393bd6768d72b5e960d442b660b37298c6
Alexandr Tulisov <shura74084@gmail.com>
2025-04-05 19:25:14 +0500

    Fix Route

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import "./index.css";
 import App from "@/App.jsx";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 // import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { ThemeProviderWrapper } from "@contexts/ThemeProviderContext.jsx";
@@ -25,7 +25,7 @@ const queryClient = new QueryClient({
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <Router basename="/react-bp">
+      <Router>
         <AuthProvider>
           <ThemeProviderWrapper>
             <ScreenProvider>
